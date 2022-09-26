754f04d39959f167a4f2dd073411202dddcbb576
furkan.kucuk <oiua66f7olp67z3wciqh7tqwi5lvok5342iw7zwrmgp5sacrur3a>
2022-09-26 16:12:14 +0300

    Add OkHttp Logger

--- app/src/main/java/com/example/mvvmdagger2/di/RetrofitModule.java
+++ app/src/main/java/com/example/mvvmdagger2/di/RetrofitModule.java
@@ -6,6 +6,8 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -23,15 +25,29 @@ public class RetrofitModule {
         return retrofit.create(IRetrofitService.class);
     }
 
+
+    @Singleton
+    @Provides
+    public HttpLoggingInterceptor provideLoggingInterceptor() {
+        return new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    }
+
+    @Singleton
+    @Provides
+    public OkHttpClient setOkhttpClient(){
+        return new OkHttpClient.Builder().addInterceptor(provideLoggingInterceptor()).build();
+    }
+
     @Singleton
     @Provides
     public Retrofit getRetrofitInstance() {
-        Retrofit retrofit = new Retrofit.Builder()
+
+        return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(setOkhttpClient())
                 .build();
-
-        return retrofit;
     }
 
 }
+
